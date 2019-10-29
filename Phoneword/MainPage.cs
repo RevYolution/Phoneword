3d13c2ca602a523369fb820061b2efbfc44576d0
Jon Rice <revyolution1120@gmail.com>
2019-10-29 12:28:45 -0700

    Translate working along with buttons

--- Phoneword/MainPage.cs
+++ Phoneword/MainPage.cs
@@ -7,6 +7,8 @@ namespace Phoneword
         Entry phoneNumberText;
         Button translateButton;
         Button callButton;
+        string translatedNumber;
+
         public MainPage()
         {
             this.Padding = new Thickness(20, 20, 20, 20);
@@ -38,7 +40,52 @@ namespace Phoneword
                 IsEnabled = false,
             });
 
+            translateButton.Clicked += OnTranslate;
+            callButton.Clicked += OnCall;
             this.Content = panel;
+        }
+
+        /// <summary>
+        /// Takes in the users entered phone number and attemps to translate it.  
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnTranslate(object sender, EventArgs e)
+        {
+            string enteredNumber = phoneNumberText.Text;
+            translatedNumber = Core.PhonewordTranslator.ToNumber(enteredNumber);
+
+            //If the string is not empty the text of the Call Button is changed to
+            //"Call whatever the translatedNumber is"
+            if (!string.IsNullOrEmpty(translatedNumber))
+            {
+                callButton.IsEnabled = true;
+                callButton.Text = "Call " + translatedNumber;
+            }
+            //If the translation returns null the button will remain disabled
+            //with no change in text.
+            else
+            {
+                callButton.IsEnabled = false;
+                callButton.Text = "Call";
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        async void OnCall(object sender, System.EventArgs e)
+        {
+            if (await this.DisplayAlert(
+                "Dial a Number",
+                "Would you like to call " + translatedNumber + "?",
+                "Yes",
+                "No"))
+            {
+
+            }
         }
     }
 }
