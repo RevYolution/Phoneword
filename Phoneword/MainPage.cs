34b196c73a943391fc5035a556c781abd6757a27
Jon Rice <revyolution1120@gmail.com>
2019-10-29 10:09:37 -0700

    Set up initial screen with no iteraction.

--- Phoneword/MainPage.cs
+++ Phoneword/MainPage.cs
@@ -4,8 +4,41 @@ namespace Phoneword
 {
     public class MainPage : ContentPage
     {
+        Entry phoneNumberText;
+        Button translateButton;
+        Button callButton;
         public MainPage()
         {
+            this.Padding = new Thickness(20, 20, 20, 20);
+
+            StackLayout panel = new StackLayout
+            {
+                Spacing = 15
+            };
+
+            panel.Children.Add(new Label
+            {
+                Text = "Enter a Phoneword",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label))
+            });
+
+            panel.Children.Add(phoneNumberText = new Entry
+            {
+                Text = "1-855-XAMARIN"
+            });
+
+            panel.Children.Add(translateButton = new Button
+            {
+                Text = "Translate"
+            });
+
+            panel.Children.Add(callButton = new Button
+            {
+                Text = "Call",
+                IsEnabled = false,
+            });
+
+            this.Content = panel;
         }
     }
 }
